21af3319aceec09facf6534375ec7bd5df480c65
Vladislav <vladislav890@gmail.com>
2023-01-03 12:35:08 +0300

    add debounce from lodash for searchInput

--- src/components/searchInput.jsx
+++ src/components/searchInput.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import searchLogo from "../assets/img/free-icon-search-3991270.png"
 import reject from "../assets/img/reject.png"
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { searchPizzaByTitle } from '../store/slices/searchSlice'
+import { debounce } from 'lodash'
 
 const SearchInput = () => {
-  const searchValue = useSelector(state => state.search.value)
+  const [value, setValue] = useState('')
+  // const searchValue = useSelector(state => state.search.value)
   const dispatch = useDispatch()
+  const inputRef = useRef()
+  const updateSearchInput = debounce((str) => {
+    dispatch(searchPizzaByTitle(str))
+  }, 500)
+  const onChangeInput = useCallback(event => {
+    setValue(event.target.value)
+    updateSearchInput(event.target.value)
+  }, [updateSearchInput])
   return (
     <div className='header__input'>
       <img src={searchLogo} alt="" className='img img-search' />
-      <input type="text" className='input' value={searchValue} onChange={event => dispatch(searchPizzaByTitle(event.target.value))} />
-      {searchValue &&
-        <img src={reject} alt="" className='img img-reject' onClick={() => dispatch(searchPizzaByTitle(''))} />}
+      <input
+        type="text"
+        className='input'
+        value={value}
+        onChange={onChangeInput}
+        ref={inputRef}
+      />
+      {value &&
+        <img
+          src={reject}
+          alt=""
+          className='img img-reject'
+          onClick={() => {
+            dispatch(searchPizzaByTitle(''))
+            setValue('')
+            inputRef.current.focus()
+          }}
+
+        />}
     </div>
   )
 }
